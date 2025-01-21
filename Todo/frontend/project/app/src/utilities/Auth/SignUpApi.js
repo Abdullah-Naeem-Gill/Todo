6d39e8c9b162d2782f01f80cf7e53da30793c7e3
Abdullah Naeem <abdullahnaeemgill1724@gmail.com>
2025-01-21 20:12:11 +0500

    Todo Project

--- Todo/frontend/project/app/src/utilities/Auth/SignUpApi.js
+++ Todo/frontend/project/app/src/utilities/Auth/SignUpApi.js
@@ -1,23 +1,6 @@
 import { handleApiRequest } from "../ApiHandling";
 import { API_URL, API_AUTH } from "../Constants";
 
-// Sign up for a regular user
-export const signupUser = async (username, password) => {
-  const formData = { username, password };
-
-  const result = await handleApiRequest(
-    `${API_URL}/register`,
-    "POST",
-    formData
-  );
-
-  if (result.success) {
-    localStorage.setItem("access_token", result.data.access_token);
-  }
-
-  return result;
-};
-
 // Sign up for an admin
 export const signupAdmin = async (username, password) => {
   const formData = { username, password };
